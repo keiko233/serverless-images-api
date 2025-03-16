4a0b86267fc39fdb061d0d5c49d1cf9e843a6b6a
keiko233 <i@elaina.moe>
2025-03-16 13:28:14 +0000

    refactor(navbar-aside): improve JSX structure and readability

--- src/app/(dashboard)/_modules/navbar-aside.tsx
+++ src/app/(dashboard)/_modules/navbar-aside.tsx
@@ -19,63 +19,63 @@ export const NavbarAside = () => {
 
   return (
     <AnimatePresence initial={false}>
-      {!isCollapsed && (
-        <motion.div
-          key="backdrop"
-          className={cn(
-            "md:hidden",
-            "backdrop-blur-xl backdrop-brightness-75",
-            "fixed inset-0 z-40 h-dvh w-full",
-          )}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{
-            duration: 0.3,
-            stiffness: 1000,
-          }}
-          onClick={toggleCollapsed}
-        />
-      )}
-
-      <aside
-        key="aside"
-        className={cn(
-          "fixed top-0 h-dvh",
-          "md:h-dvh-subtract-16 md:absolute",
-          "z-50 min-w-60",
-          "bg-primary-container/30 dark:bg-on-secondary-container/30",
-          "overflow-x-hidden overflow-y-auto p-2",
-          "flex flex-col gap-1",
-          "transition-all duration-300 ease-in-out",
-          isCollapsed ? "-translate-x-60" : "",
+      <>
+        {!isCollapsed && (
+          <motion.div
+            className={cn(
+              "md:hidden",
+              "backdrop-blur-xl backdrop-brightness-75",
+              "fixed inset-0 z-40 h-dvh w-full",
+            )}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{
+              duration: 0.3,
+              stiffness: 1000,
+            }}
+            onClick={toggleCollapsed}
+          />
         )}
-      >
-        <div
+
+        <aside
           className={cn(
-            "md:hidden",
-            "text-primary flex h-12 items-center justify-center font-bold",
+            "fixed top-0 h-dvh",
+            "md:h-dvh-subtract-16 md:absolute",
+            "z-50 min-w-60",
+            "bg-primary-container/30 dark:bg-on-secondary-container/30",
+            "overflow-x-hidden overflow-y-auto p-2",
+            "flex flex-col gap-1",
+            "transition-all duration-300 ease-in-out",
+            isCollapsed ? "-translate-x-60" : "",
           )}
         >
-          Serverless Images API
-        </div>
+          <div
+            className={cn(
+              "md:hidden",
+              "text-primary flex h-12 items-center justify-center font-bold",
+            )}
+          >
+            Serverless Images API
+          </div>
 
-        {Object.entries(LINKS).map(([key, value]) => {
-          const isCurrent = pathname === value;
+          {Object.entries(LINKS).map(([key, value]) => {
+            const isCurrent = pathname === value;
 
-          return (
-            <Link href={{ pathname: value }} key={key} className="w-full">
-              <Button
-                className="w-full px-6 text-left capitalize"
-                variant={isCurrent ? "flat" : "basic"}
-                onClick={toggleCollapsed}
-              >
-                {key}
-              </Button>
-            </Link>
-          );
-        })}
-      </aside>
+            return (
+              <Link href={{ pathname: value }} key={key} className="w-full">
+                <Button
+                  className="w-full px-6 text-left capitalize"
+                  variant={isCurrent ? "flat" : "basic"}
+                  onClick={toggleCollapsed}
+                >
+                  {key}
+                </Button>
+              </Link>
+            );
+          })}
+        </aside>
+      </>
     </AnimatePresence>
   );
 };
